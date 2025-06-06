8601a2c9c9f3d28079811102f8190a678c359429
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-06 22:47:09 +0200

    Separate apis for bg removal and color determination

--- src/package_wielkate/main/models/Color.py
+++ src/package_wielkate/main/models/Color.py
@@ -1,7 +1,5 @@
 from colorsys import rgb_to_hsv
 
-from skimage.color import rgb2lab
-
 
 class Color:
     def __init__(self, row):
@@ -9,7 +7,6 @@ class Color:
         self.r = int(row[1]) / 255
         self.g = int(row[2]) / 255
         self.b = int(row[3]) / 255
-        self.lab = rgb2lab([self.r, self.g, self.b])
         self.hsv = self.__get_hsv_in_degrees_and_percentage__()
 
     def __get_hsv_in_degrees_and_percentage__(self):
