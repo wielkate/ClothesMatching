6a3566b8d3e81307a49de1415f33f8a84f1d774c
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-22 11:42:18 +0200

    SearchBar --> custom list

--- src/package_wielkate/main/ui/DisplayCard.py
+++ src/package_wielkate/main/ui/DisplayCard.py
@@ -7,9 +7,7 @@ from flet.core.gradients import LinearGradient
 from flet.core.icon_button import IconButton
 from flet.core.icons import Icons
 from flet.core.image import Image
-from flet.core.list_tile import ListTile
 from flet.core.row import Row
-from flet.core.search_bar import SearchBar
 from flet.core.stack import Stack
 from flet.core.text import Text
 from flet.core.types import (ClipBehavior,
@@ -20,31 +18,11 @@ from flet.core.types import (ClipBehavior,
                              TextAlign,
                              ScrollMode)
 
+from src.package_wielkate.main.models.Mode import Mode
+from src.package_wielkate.main.ui.OptionsList import OptionsList
 from src.package_wielkate.main.commons.constants import IMAGES_DIRECTORY
 from src.package_wielkate.main.commons.global_clothes import global_clothes
 from src.package_wielkate.main.commons.global_colors import global_colors
-from src.package_wielkate.main.models.Mode import Mode
-
-
-def _create_search_bar(items, on_select, on_back):
-    return SearchBar(
-        visible=False,
-        view_leading=IconButton(
-            icon=Icons.ARROW_BACK_ROUNDED,
-            tooltip="Back",
-            on_click=on_back
-        ),
-        controls=[
-            ListTile(
-                title=Text(
-                    value=item,
-                    text_align=TextAlign.CENTER
-                ),
-                data=item,
-                on_click=on_select)
-            for item in items
-        ]
-    )
 
 
 class DisplayCard(Column):
@@ -58,18 +36,26 @@ class DisplayCard(Column):
         self.edit_card_action = edit_card_action
         self.return_clothes_action = return_clothes_action
 
-        self.color_options = _create_search_bar(
+        self.color_options = OptionsList(
             items=[color.name for color in global_colors],
             on_select=self._select_color,
-            on_back=lambda e: self._close_search_bar(self.color_options)
+            on_back=lambda e: self._close_options(self.color_options)
         )
-        self.mode_options = _create_search_bar(
+        self.mode_options = OptionsList(
             items=[mode.value for mode in Mode],
             on_select=self._select_mode,
-            on_back=lambda e: self._close_search_bar(self.mode_options)
+            on_back=lambda e: self._close_options(self.mode_options)
         )
         self.display_card = self._create_display_card()
-        self.controls = [self.display_card, self.color_options, self.mode_options]
+        self.controls = [
+            Stack(
+                controls=[
+                    self.display_card,
+                    self.color_options,
+                    self.mode_options
+                ]
+            )
+        ]
 
     def _create_display_card(self):
         return Container(
@@ -132,12 +118,12 @@ class DisplayCard(Column):
                                         IconButton(
                                             icon=Icons.CREATE_ROUNDED,
                                             tooltip="Edit color name",
-                                            on_click=lambda e: self._open_search_bar(self.color_options),
+                                            on_click=lambda e: self._open_options(self.color_options),
                                         ),
                                         IconButton(
                                             icon=Icons.DONE_ROUNDED,
                                             tooltip="Choose item",
-                                            on_click=lambda e: self._open_search_bar(self.mode_options)
+                                            on_click=lambda e: self._open_options(self.mode_options)
                                         ),
                                         IconButton(
                                             icon=Icons.DELETE_OUTLINE_ROUNDED,
@@ -153,14 +139,12 @@ class DisplayCard(Column):
             ),
         )
 
-    def _open_search_bar(self, search_bar):
-        search_bar.visible = True
-        search_bar.open_view()
+    def _open_options(self, options_list):
+        options_list.visible = True
         self.update()
 
-    def _close_search_bar(self, search_bar):
-        search_bar.close_view()
-        search_bar.visible = False
+    def _close_options(self, options_list):
+        options_list.visible = False
         self.update()
 
     def _get_matched_items_by(self, mode):
@@ -176,18 +160,16 @@ class DisplayCard(Column):
 
     def _select_color(self, e):
         selected_color = e.control.data
-        self.display_card.content.controls[2].content.controls[0].value = selected_color
         self.color_name = selected_color
-        self.color_options.close_view(selected_color)
-        self.color_options.visible = False
+        self.display_card.content.controls[2].content.controls[0].value = selected_color
+        self._close_options(self.color_options)
         self.edit_card_action(self)
         self.update()
 
     def _select_mode(self, e):
-        mode = e.control.data
-        self.mode_options.close_view(mode)
-        self.mode_options.visible = False
-        self.return_clothes_action(self._get_matched_items_by(mode))
+        selected_mode = e.control.data
+        self._close_options(self.mode_options)
+        self.return_clothes_action(self._get_matched_items_by(selected_mode))
         self.update()
 
     def _delete_clicked(self, e):
---
+++ src/package_wielkate/main/ui/OptionsList.py
@@ -0,0 +1,54 @@
+from flet.core import border
+from flet.core.colors import Colors
+from flet.core.column import Column
+from flet.core.container import Container
+from flet.core.icon_button import IconButton
+from flet.core.icons import Icons
+from flet.core.list_tile import ListTile
+from flet.core.text import Text
+from flet.core.types import ClipBehavior, ScrollMode, TextAlign
+
+
+class OptionsList(Container):
+    def __init__(self, items, on_select, on_back):
+        super().__init__(
+            visible=False,
+            expand=True,
+            width=323,
+            height=636,
+            border_radius=10,
+            padding=10,
+            clip_behavior=ClipBehavior.HARD_EDGE,
+            border=border.all(
+                width=1,
+                color=Colors.WHITE
+            ),
+            bgcolor=Colors.BLACK,
+            content=Column(
+                controls=[
+                    IconButton(
+                        icon=Icons.ARROW_BACK_ROUNDED,
+                        tooltip="Back",
+                        on_click=on_back
+                    ),
+                    # Scrollable list area
+                    Container(
+                        expand=True,
+                        content=Column(
+                            scroll=ScrollMode.AUTO,
+                            controls=[
+                                *[
+                                    ListTile(
+                                        title=Text(item, text_align=TextAlign.CENTER),
+                                        data=item,
+                                        on_click=on_select
+                                    )
+                                    for item in items
+                                ]
+                            ]
+                        )
+                    )
+                ],
+                expand=True
+            )
+        )
