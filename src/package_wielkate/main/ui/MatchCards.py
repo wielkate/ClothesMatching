7c885d7cae21ff39f4e15d6bcd76f55fdef78d4f
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-06-23 04:53:44 +0200

    Add database

--- src/package_wielkate/main/ui/MatchCards.py
+++ src/package_wielkate/main/ui/MatchCards.py
@@ -1,7 +1,8 @@
 from flet.core.column import Column
 from flet.core.types import ScrollMode
 
-from src.package_wielkate.main.commons.global_combinations import global_monochrome, global_complementary, global_analogous
+from src.package_wielkate.main.commons.global_combinations import global_monochrome, global_complementary, \
+    global_analogous
 from src.package_wielkate.main.models.Clothes import Clothes
 from src.package_wielkate.main.ui.MatchCard import MatchCard
 
@@ -12,12 +13,12 @@ class MatchCards(Column):
             scroll=ScrollMode.HIDDEN,
         )
         self.clothes = Clothes()
-        self.list = self._load_clothes_from_memory()
+        self.list = self.load_clothes_from_memory()
 
-    def _load_clothes_from_memory(self):
+    def load_clothes_from_memory(self):
         return [MatchCard(item[0], item[1]) for item in self.clothes.load_clothes()]
 
-    def _get_related_colors(self, related_colors, filename, color_name):
+    def get_related_colors(self, related_colors, filename, color_name):
         return [item for item in self.list if
                 item.color_name in related_colors[color_name] and item.filename != filename]
 
@@ -39,10 +40,10 @@ class MatchCards(Column):
         self.list = [l for l in self.list if l.filename != filename]
 
     def get_monochrome_for(self, filename, color_name):
-        return self._get_related_colors(global_monochrome, filename, color_name)
+        return self.get_related_colors(global_monochrome, filename, color_name)
 
     def get_analogous_colors_for(self, filename, color_name):
-        return self._get_related_colors(global_analogous, filename, color_name)
+        return self.get_related_colors(global_analogous, filename, color_name)
 
     def get_complementary_colors_for(self, filename, color_name):
-        return self._get_related_colors(global_complementary, filename, color_name)
+        return self.get_related_colors(global_complementary, filename, color_name)
