26181959f1e85e2f1bcbd09999af0393bc6925c2
Ekaterina Mashchenko <mashchenko.kate@gmail.com>
2025-08-16 02:07:34 +0200

    READ.me update

---
+++ src/package_wielkate/main/resources/auth.py
@@ -0,0 +1,8 @@
+REMOVE_BG_API_KEY = 'px6lvdyzar8vxgf'
+REMOVE_BG_API_PASS = 'fk4p8hfda84l9ojpk3v61vsr32q9h303ps8tk59kut6cc6om27lc'
+REMOVE_BG_API = 'https://api.pixian.ai/api/v2/remove-background'
+
+BUCKET_URL = 'https://qpzqxqiyzpwaowwmafvv.supabase.co/storage/v1/object/public/images/'
+
+CLOTHES_MATCHING_API = 'https://clothes-matching-api-eu.onrender.com/v1'
+# CLOTHES_MATCHING_API = 'http://127.0.0.1:8000'
